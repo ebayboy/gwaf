1f91109b06da054b68da2a859ad6a8f06b1d60ba
fanpengfei <fanpengfei@jd.com>
2018-06-28 04:40:42 -0400

    add vendor libs.

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/cihub/seelog"
+	"github.com/flier/gohs/hyperscan"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"github.com/golang/groupcache"
+	proto "github.com/golang/protobuf/proto"
 	"github.com/kardianos/service"
 	"github.com/valyala/fasthttp"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
 )
 
 func main() {
