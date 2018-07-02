1799cbcbda38412be93a95f833fa18a00da9d5a8
fanpengfei <fanpengfei@jd.com>
2018-07-01 22:19:00 -0400

    update for daemon

---
+++ daemon/README.md
@@ -0,0 +1,5 @@
+
+
+1. 编译hyperscan为动态库形势， 并安装， 
+2. 在/etc/profile后增加： export PKG_CONFIG_PATH=/usr/share/pkgconfig:/usr/lib/pkgconfig:/usr/local/lib64/pkgconfig
+3. ln -s /usr/bin/pkg-config /usr/local/bin/pkg-config
--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/cihub/seelog"
-	"github.com/flier/gohs/hyperscan"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/go-xorm/xorm"
-	"github.com/golang/groupcache"
-	proto "github.com/golang/protobuf/proto"
-	"github.com/kardianos/service"
-	"github.com/valyala/fasthttp"
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
-)
+	/*
+		"github.com/flier/gohs/hyperscan"
+		"database/sql"
+		"github.com/cihub/seelog"
+		_ "github.com/go-sql-driver/mysql"
+		"github.com/go-xorm/xorm"
+		"github.com/golang/groupcache"
+		proto "github.com/golang/protobuf/proto"
+		"github.com/kardianos/service"
+		"github.com/valyala/fasthttp"
+		context "golang.org/x/net/context"
+	*/)
 
 func main() {
 	fmt.Println("vim-go")
